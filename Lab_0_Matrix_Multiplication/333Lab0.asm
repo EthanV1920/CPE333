9832b45fb2cdb891e749f689fceacad8261e41a1
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-04-04 15:04:44 -0700

    lab session

--- Lab_0_Matrix_Multiplication/333Lab0.asm
+++ Lab_0_Matrix_Multiplication/333Lab0.asm
@@ -1,5 +1,5 @@
 ###########################################################################
-#################### CPE 333 LAB 0   ######################################
+#################### CPE 333 LAB 0 ########################################
 ###########################################################################
 #Isaac Lake, Ethan Vosburg, Samuel Solano, and Victoria Asencio-Clemens
 
@@ -9,16 +9,88 @@ A:      .word 0,   3,   2,   0,   3,   1,   0,   3,   2
 
 B:      .word 1,   1,   0,   3,   1,   2,   0,   0,   0
 
-C:      .word 0,   0,  0,   0,   0,   0,   0,   0,   0
+C:      .word 0,   0,   0,   0,   0,   0,   0,   0,   0
 
-dim_size: .word 3
+
 
 .text
+###########################################################################
+#################### Initialization  ######################################
+###########################################################################
+
         li sp, 0x10000          # Initialize the stack pointer
         la s0, A                # Load the base address of A into s0
         la s1, B                # Load the base address of B into s1
         la s2, C                # Load the base address of C into s2
-        la s3, dim_size         # Load the base address of dim_size into s3
+        li s3, 3                # Load the dim_size into s3
+
+###########################################################################
+#################### Matrix Traversal Function  ###########################
+###########################################################################
+
+#a0 row number
+#a1 column number
+#a2 return value
+
+
+		addi a0, x0, -1          # row number to input into RCMULT
+		addi a1, x0, 0          # sentinel for overall loop  and  - col number
+		addi t0, x0, 0          # sentinel for nested loop  - row loop
+                addi t1, x0, 0          # this is the counter for the array in jumps of 4 (for a word)
+		
+
+LOOP:   beq t0, s3, DONE
+		addi a1, x0, 0          # sentinel for nested loop
+		addi a0, a0, 1
+                addi t0, t0, 1
+
+
+NESTED: beq a1, s3, LOOP        
+		
+		call RCMULT
+
+                add s4, s2, t1             #change s4 to store location <- (s2 (array start) + t1 (offset))
+                sw a2, 0(s4)               # put mult result into s4
+                addi t1, t1, 4             #update t1 which is the array offset
+
+		addi a1, a1, 1             #update nested sentinel 
+		j NESTED
+
+
+	# lw a6, 0(s0)            # loads first value of matrix A into a6
+	# lw a7, 0(s1)            # loads first value of matrix B into a7
+                 
+	# call MULT               # multiplies matrix A and B values
+
+	# sw a7, 0(s2)            # store the product in matrix C address
+
+###########################################################################
+#################### Row / Column Multiplication  #########################
+###########################################################################
+
+#Inputs: a0, a1
+#Outputs: a2
+#Registers Changed: t4, t5, t6, a3, a4
+RCMULT:
+addi sp, sp, -4
+sw ra, 0(sp)
+mv a6, a0
+mv a7, s3
+call MULT
+slli t4, a7, 2
+add t4, s0, t4
+mv t5, a1
+slli t5, t5, 2
+mv t6, zero
+mv a2, zero
+bge t4, s3, RCMULTEND
+lw a3, 0(t4)
+lw a4, 0(t5)
+mv a6, a3
+mv a7, a4
+call MULT
+
+
 
 ###########################################################################
 #################### Multiplication Function  #############################
@@ -51,3 +123,6 @@ MULTEND:
 	lw ra, 0(sp)		#Pop return address from the stack
 	addi sp, sp, 4
 	ret
+
+
+DONE: nop
