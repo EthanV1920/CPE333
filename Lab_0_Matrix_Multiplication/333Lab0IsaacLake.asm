7c458e84e73f0e950154e0a5fabb03ee514b22dd
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-04-03 23:26:35 -0700

    Oops that loop was infinite

--- Lab_0_Matrix_Multiplication/333Lab0IsaacLake.asm
+++ Lab_0_Matrix_Multiplication/333Lab0IsaacLake.asm
@@ -1,13 +1,14 @@
 ###########################################################################
 #################### Multiplication Function  #############################
 ###########################################################################
+#Note only works for possitive integers
 
 #Inputs: a6, a7
 #Outputs: a7
 #Registers Changed: t0, t1, t2, t3
 MULT:	
 	addi sp, sp, -4		#Pushes ra to stack
-    	sw ra, 0(sp)
+    sw ra, 0(sp)
 	li t0, 0		#Running Total
 	li t1, 1		#Loop "Counter"
 	li t2, -1		#Starts at -1 Due to how we increment it
@@ -16,6 +17,7 @@ MULTI:
 	beqz t1, MULTEND	#Checks if we are finished with the mult
 	and t3, t1, a7		#Checks if we should add this loop
 	addi t2, t2, 1
+	slli t1, t1, 1
 	beqz t3, MULTI
 	sll t3, a6, t2		
 	add t0, t0, t3
