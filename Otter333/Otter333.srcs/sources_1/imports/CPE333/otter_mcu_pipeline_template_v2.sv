7953ed160d52b30506592df79f75936777b2e01e
IsaacLake03 <156034639+IsaacLake03@users.noreply.github.com>
2024-04-24 17:15:33 -0700

    some work done in decoder

--- Otter333/Otter333.srcs/sources_1/imports/CPE333/otter_mcu_pipeline_template_v2.sv
+++ Otter333/Otter333.srcs/sources_1/imports/CPE333/otter_mcu_pipeline_template_v2.sv
@@ -79,8 +79,7 @@ module OTTER_MCU(input CLK,
      logic [31:0] if_de_reg;
      
      always_ff @(posedge CLK) begin
-                if_de_pc <= pc;
-                
+            if_de_pc <= pc;
      end
      
      assign PC_WE = 1'b1; 	//Hardwired high, assuming no hazards
@@ -94,6 +93,7 @@ module OTTER_MCU(input CLK,
                          .MEM_RDEN1(cu_memRDEN1), .MEM_RDEN2(cu_memRDEN2), .MEM_WE2(cu_memWE2), 
                          .MEM_SIZE(ir[13:12]), .MEM_SIGN(ir[14]), .MEM_DOUT2(ir), .MEM_DOUT1(if_de_reg), 
                          .IO_IN(IOBUS_IN), .IO_WR(IOBUS_WR), .MEM_CLK(CLK));
+    
 
     
      
@@ -136,14 +136,14 @@ module OTTER_MCU(input CLK,
    CU_Decoder otter_cu_dec (.ir(if_de_reg), .br_EQ(cu_br_eq), .br_LT(cu_br_lt), .br_LTU(cu_br_ltu), .CU_RST(cu_reset), .ALU_FUN(cu_alu_fun), .srcA_SEL(mux_a_sel), 
                                 .srcB_SEL(mux_b_sel), .PC_SEL(cu_pc_sel), .RF_SEL(cu_rf_sel), .RF_WE(cu_rf_we), .memWE2(cu_memWE2), .memRDEN2(cu_RDEN2));
 
-    RegFile otter_reg_file (.en(cu_rf_we), .adr1(if_de_reg[19:15]), .adr2(if_de_reg[24:20]), .w_adr(if_de_reg[11:7]), 
+    RegFile otter_reg_file (.en(cu_rf_we), .adr1(if_de_reg[19:15]), .adr2(if_de_reg[24:20]), .w_adr(), 
                             .w_data(RF_MUX_out), .CLK(CLK), .rs1(sreg1), .rs2(sreg2));
     
     Immed_Gen otter_immed_gen (.Instruction(if_de_reg), .U_Type(U_imm), .I_Type(I_imm), .S_Type(S_imm), 
                                .B_Type(B_imm), .J_Type(J_imm));
                                
                                
-   ALU_MUX_A otter_alu_muxA (.ALUmuxA0(sreg1), .ALUmuxA1(U_imm), .ALUmuxA2(~sreg1), 
+    ALU_MUX_A otter_alu_muxA (.ALUmuxA0(sreg1), .ALUmuxA1(U_imm), .ALUmuxA2(~sreg1), 
                               .ALUmuxA_SEL(mux_a_sel), .ALUmuxA_out(mux_a_out));
     
     ALU_MUX_B otter_alu_muxB (.ALUmuxB0(sreg2), .ALUmuxB1(I_imm), .ALUmuxB2(S_imm), .ALUmuxB3(pc_out), 
@@ -153,13 +153,14 @@ module OTTER_MCU(input CLK,
    intr_t id_ex_struct;
    
    always_ff @ (posedge CLK) begin
-        id_ex_struct.regWrite = cu_rf_we
-        id_ex_struct.memWrite = cu_memWE2
-        id_ex_struct.memRead2 = cu_RDEN2
-        id_ex_struct.alu_fun = cu_alu_fun
-        id_ex_struct.rf_wr_sel = cu_rf_sel
+        id_ex_struct.regWrite = cu_rf_we;
+        id_ex_struct.memWrite = cu_memWE2;
+        id_ex_struct.memRead2 = cu_RDEN2;
+        id_ex_struct.alu_fun = cu_alu_fun;
+        id_ex_struct.rf_wr_sel = cu_rf_sel;
         id_ex_struct.mux_A_out = mux_a_out;
         id_ex_struct.mux_B_out = mux_b_out;
+        id_ex_struct.rd_addr 
    end
     
     
@@ -172,12 +173,14 @@ module OTTER_MCU(input CLK,
      logic [31:0] opA_forwarded;
      logic [31:0] opB_forwarded;
      
-     // Creates a RISC-V ALU
+    // Creates a RISC-V ALU
     //OTTER_ALU ALU (de_ex_inst.alu_fun, de_ex_opA, de_ex_opB, aluResult); // the ALU (from diego)
+
+
      
 
     
-    ALU otter_ALU (.srcA(mux_a_out), .srcB(mux_b_out), .alu_fun(cu_alu_fun), .alu_result(alu_out));
+    ALU otter_ALU (.srcA(id_ex_struct.mux_A_out), .srcB(id_ex_struct.mux_B_out), .alu_fun(id_ex_struct.alu_fun), .alu_result(id_ex_struct.regWrite));
     
     Branch_Condition_Generator otter_cond_gen (.br1(sreg1), .br2(sreg2), .br_eq(cu_br_eq), 
                                                .br_lt(cu_br_lt), .br_ltu(cu_br_ltu));
@@ -185,6 +188,25 @@ module OTTER_MCU(input CLK,
     Branch_Address_Generator otter_addr_gen (.B_Type(B_imm), .J_Type(J_imm), .I_Type(I_imm), 
                                              .PC(if_de_pc), .rs(sreg1), .branch(addr_pc_branch), 
                                              .jal(addr_pc_jal), .jalr(addr_pc_jalr));
+
+    always_ff @ (posedge CLK) begin
+        ex_mem_inst.opcode = id_ex_struct.opcode;
+        ex_mem_inst.rs1_addr = id_ex_struct.rs1_addr;
+        ex_mem_inst.rs2_addr = id_ex_struct.rs2_addr;
+        ex_mem_inst.rd_addr = id_ex_struct.rd_addr;
+        ex_mem_inst.rs1_used = id_ex_struct.rs1_used;
+        ex_mem_inst.rs2_used = id_ex_struct.rs2_used;
+        ex_mem_inst.rd_used = id_ex_struct.rd_used;
+        ex_mem_inst.alu_fun = id_ex_struct.alu_fun;
+        ex_mem_inst.memWrite = id_ex_struct.memWrite;
+        ex_mem_inst.memRead2 = id_ex_struct.memRead2;
+        ex_mem_inst.regWrite = id_ex_struct.regWrite;
+        ex_mem_inst.rf_wr_sel = id_ex_struct.rf_wr_sel;
+        ex_mem_inst.mem_type = id_ex_struct.mem_type;  //sign, size
+        ex_mem_inst.pc = id_ex_struct.pc;
+        ex_mem_inst.mux_A_out = id_ex_struct.mux_A_out;
+        ex_mem_inst.mux_B_out = id_ex_struct.mux_B_out;
+    end
     
    
 
@@ -202,8 +224,11 @@ module OTTER_MCU(input CLK,
  
      
 //==== Write Back ==================================================
+
+
+
      
-   RF_MUX otter_rf_mux (.RFmux0(pc_out), .RFmux1(csr_rd), .RFmux2(out_data2), .RFmux3(alu_out), 
+RF_MUX otter_rf_mux (.RFmux0(pc_out), .RFmux1(csr_rd), .RFmux2(out_data2), .RFmux3(alu_out), 
                          .RFmux_SEL(cu_rf_sel), .RFmux_out(RF_MUX_out));
 
 
