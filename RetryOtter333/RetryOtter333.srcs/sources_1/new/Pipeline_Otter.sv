38c795262cab1da200c9f831e62cdffe3b1dbf85
Ethan Vosburg <ethan@vosburg.us>
2024-05-20 11:59:25 -0700

    Fixing Issacs Issues

--- RetryOtter333/RetryOtter333.srcs/sources_1/new/Pipeline_Otter.sv
+++ RetryOtter333/RetryOtter333.srcs/sources_1/new/Pipeline_Otter.sv
@@ -8,47 +8,6 @@
 // Target Devices: Basys 3
 //////////////////////////////////////////////////////////////////////////////////
 
-typedef enum logic [6:0] {
-  LUI    = 7'b0110111,
-  AUIPC  = 7'b0010111,
-  JAL    = 7'b1101111,
-  JALR   = 7'b1100111,
-  BRANCH = 7'b1100011,
-  LOAD   = 7'b0000011,
-  STORE  = 7'b0100011,
-  OP_IMM = 7'b0010011,
-  OP     = 7'b0110011,
-  SYSTEM = 7'b1110011,
-  DEFAULT = 7'b0000000
-} opcode_t;
-
-typedef struct packed {
-  logic [31:0] pc;
-  opcode_t opcode;
-  logic [4:0] rs1_addr;
-  logic [4:0] rs2_addr;
-  logic [4:0] rd_addr;
-  logic [3:0] alu_fun;
-  logic memWrite;
-  logic memRead2;
-  logic regWrite;
-  logic [1:0] rf_SEL;
-  // added by v and sosa
-  logic [31:0] ir;
-  logic [31:0] memDout2;
-  logic [31:0] JALR;
-  logic [31:0] BRANCH;
-  logic [31:0] JAL;
-  logic [31:0] alu_result;
-  logic [31:0] rs1;
-  logic [31:0] rs2;
-  logic [2:0] PC_SEL;
-  logic [31:0] IOBUS_in;
-  logic IOBUS_wr;
-  logic [31:0] muxB_out;
-  logic [31:0] muxA_out;
-//still need mtvec and sys location, plus pc write and memrden1
-} instr_t;
 
 module Pipeline_Otter (
     input logic CLK,
@@ -60,12 +19,13 @@ module Pipeline_Otter (
     output logic IOBUS_WR
 );
 
-  logic cu_br_eq, cu_br_lt, cu_br_ltu, cu_memRDEN1;
+  logic cu_br_eq, cu_br_lt, cu_br_ltu, cu_memRDEN1, FmuxASel, FmuxBSel, FlushFlag, PC_WE;
                 
   logic [31:0] csr_pc_mepc, csr_pc_mtvec, csr_rd, U_imm, I_imm,
-               S_imm, B_imm, J_imm, addr_pc_jal, addr_pc_branch, addr_pc_jalr;
+               S_imm, B_imm, J_imm, addr_pc_jal, addr_pc_branch, addr_pc_jalr,
+               FmuxA, FmuxB, MuxA_out, MuxB_out;
                
-  logic [2:0] cu_pc_sel;
+  logic [2:0] cu_pc_sel, flushCount;
   
   
   //added by me
@@ -73,30 +33,34 @@ module Pipeline_Otter (
   logic [2:0] muxB_SEL;
   logic [1:0] muxA_SEL;
   logic [31:0] pcp4Holder;
-  logic intTaken;
   
     instr_t fetch_struct;
     instr_t decode_struct;
     instr_t execute_struct;
     instr_t mem_struct;
     instr_t write_struct;
+    instr_t HDU_fetch_out;
 
   //==== Instruction Fetch ===========================================
-  assign PC_WE  = 1'b1;  //Hardwired high, assuming no hazard
+    always_comb begin
+        if(RESET == 1'b1)begin
+            PC_WE = 1'b1;
+        end
+    end
 
     PCTOP MyPCTOP(
        .PC_RST(RESET),
        .PC_WE(PC_WE), 
        .JALR(addr_pc_jalr),
        .BRANCH(addr_pc_branch), 
-       .JAL(ddr_pc_jal),
-       .PC_SEL(Pcu_pc_sel),
+       .JAL(addr_pc_jal),
+       .PC_SEL(execute_struct.PC_SEL),
        .CLK(CLK), 
        .MTVEC(csr_pc_mtvec),
        .MEPC(csr_pc_mepc),
        //outs
        .PC(fetch_struct.pc),
-       .PCplus4(PC4)
+       .PCplus4(pcp4Holder)
       );
 
 
@@ -165,7 +129,7 @@ module Pipeline_Otter (
 //        decode_struct <= fetch_struct;
 //      end
       
-            always_ff @(posedge CLK) begin
+    always_ff @(posedge CLK) begin
         decode_struct.pc <= fetch_struct.pc;
         decode_struct.opcode <= fetch_struct.opcode;
         decode_struct.rs1_addr <= fetch_struct.rs1_addr;
@@ -201,6 +165,25 @@ module Pipeline_Otter (
       .memWrite(tempMemWrite),
       .memRDEN2(tempMemRead)
   );
+  
+HDU otterHazards(
+    .ID(fetch_struct),
+    .EX(execute_struct),
+    .MEM(memory_struct),
+    .WB(write_struct),
+    .FmuxA(FmuxA),
+    .FmuxB(FmuxB),
+    .FmuxASel(FmuxASel),
+    .FmuxBSel(FmuxBSel),
+    .PC_WE(PC_WE),
+    .FlushFlag(FlushFlag),
+    .ID_out(HDU_fetch_out)
+);
+    always_comb begin
+        if(FlushFlag == 1)begin
+            flushCount = 2'b10;
+        end
+    end
 
     assign decode_struct.alu_fun = tempFun;
     assign decode_struct.memWrite = tempMemWrite; // create
@@ -250,7 +233,7 @@ module Pipeline_Otter (
       .ALUmuxA2(~decode_struct.rs1),
       .ALUmuxA_SEL(muxA_SEL),
       //outs
-      .ALUmuxA_out(decode_struct.muxA_out)
+      .ALUmuxA_out(MuxA_out)
   );
   
 
@@ -262,7 +245,21 @@ module Pipeline_Otter (
       .ALUmuxB4(csr_rd),
       .ALUmuxB_SEL(muxB_SEL),
       //outs
-      .ALUmuxB_out(decode_struct.muxB_out)
+      .ALUmuxB_out(MuxB_out)
+  );
+  
+  MUX ForwardmuxA (
+    .in1(MuxA_out),
+    .in2(FmuxA),
+    .MuxSel(FmuxASel),
+    .MuxOut(decode_struct.muxA_out)
+  );
+  
+  MUX ForwardmuxB (
+    .in1(MuxB_out),
+    .in2(FmuxB),
+    .MuxSel(FmuxBSel),
+    .MuxOut(decode_struct.muxB_out)
   );
   
 //  always_comb begin
@@ -282,20 +279,20 @@ module Pipeline_Otter (
 //     end
      
      always_ff @(posedge CLK) begin
-              execute_struct.pc <= decode_struct.pc;
-          execute_struct.opcode <= decode_struct.opcode;
+        execute_struct.pc <= decode_struct.pc;
+        execute_struct.opcode <= decode_struct.opcode;
         execute_struct.rs1_addr <= decode_struct.rs1_addr;
         execute_struct.rs2_addr <= decode_struct.rs2_addr;
-         execute_struct.rd_addr <= decode_struct.rd_addr;
+        execute_struct.rd_addr <= decode_struct.rd_addr;
         execute_struct.memRead2 <= decode_struct.memRead2;
         execute_struct.regWrite <= decode_struct.regWrite;
-              execute_struct.ir <= decode_struct.ir;
+        execute_struct.ir <= decode_struct.ir;
         execute_struct.memDout2 <= decode_struct.memDout2;
-            execute_struct.JALR <= decode_struct.JALR;
-          execute_struct.BRANCH <= decode_struct.BRANCH;
-             execute_struct.JAL <= decode_struct.JAL;
-             execute_struct.rs1 <= decode_struct.rs1;
-          execute_struct.PC_SEL <= decode_struct.PC_SEL;
+        execute_struct.JALR <= decode_struct.JALR;
+        execute_struct.BRANCH <= decode_struct.BRANCH;
+        execute_struct.JAL <= decode_struct.JAL;
+        execute_struct.rs1 <= decode_struct.rs1;
+        execute_struct.PC_SEL <= decode_struct.PC_SEL;
         execute_struct.IOBUS_in <= decode_struct.IOBUS_in;
         execute_struct.IOBUS_wr <= decode_struct.IOBUS_wr;
         execute_struct.alu_fun <= decode_struct.alu_fun;
@@ -379,7 +376,35 @@ module Pipeline_Otter (
   //==== Write Back ==================================================
    
    always_ff @(posedge CLK) begin
-        write_struct <= mem_struct;
+            write_struct.pc <= mem_struct.pc;
+            write_struct.opcode <= mem_struct.opcode;
+            write_struct.rs1_addr <= mem_struct.rs1_addr;
+            write_struct.rs2_addr <= mem_struct.rs2_addr;
+            write_struct.rd_addr <= mem_struct.rd_addr;
+            write_struct.memRead2 <= mem_struct.memRead2;
+            write_struct.ir <= mem_struct.ir;
+            write_struct.memDout2 <= mem_struct.memDout2;
+            write_struct.JALR <= mem_struct.JALR;
+            write_struct.BRANCH <= mem_struct.BRANCH;
+            write_struct.JAL <= mem_struct.JAL;
+            write_struct.rs1 <= mem_struct.rs1;
+            write_struct.PC_SEL <= mem_struct.PC_SEL;
+            write_struct.IOBUS_in <= mem_struct.IOBUS_in;
+            write_struct.IOBUS_wr <= mem_struct.IOBUS_wr;
+            write_struct.alu_fun <= mem_struct.alu_fun;
+            write_struct.memWrite <= mem_struct.memWrite;
+            write_struct.rf_SEL <= mem_struct.rf_SEL;
+            write_struct.muxA_out <= mem_struct.muxA_out;
+            write_struct.muxB_out <= mem_struct.muxB_out;
+            write_struct.rs2 <= mem_struct.rs2;
+            write_struct.alu_result <= mem_struct.alu_result;
+            if(flushCount>0)begin
+                write_struct.regWrite <= 1'b0;
+                flushCount-=1;
+            end
+            else begin
+                write_struct.regWrite <=1'b1; 
+            end
      end
 
     
